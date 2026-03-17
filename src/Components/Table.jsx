f06055f1ccb07a9cca317059825338bcc19b63b5
Sandeep C <sandycdeep@gmail.com>
2026-03-17 15:50:55 +0530

    Updated the table and css

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import useFetch from "../CustomHooks/useFetch";
+import "../Components/Table.css";
 const Table = () => {
   const { data, message } = useFetch("https://fakestoreapi.com/products");
   console.log(data);
   return (
     <div className="main-div">
       <table border="1">
-        <thead>
+        <thead className="main-head">
           <tr>
             <th>Title</th>
             <th>Description</th>
@@ -16,7 +17,7 @@ const Table = () => {
             <th>Total price</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="main-body">
           {data.length > 0 &&
             data.map((item, index) => (
               <tr key={item.id}>
